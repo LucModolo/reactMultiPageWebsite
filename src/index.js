36be587a4cbdda8d2483b84b15ae9d76f31a755a
Luc Modolo <l.modolo@rafisa.ch>
2021-11-17 09:55:37 +0100

    Command out Test

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import {
   Footer,
   Home,
   About,
+  Mysite,
   Contact,
   Blog,
   Posts,
@@ -21,6 +22,7 @@ ReactDOM.render(
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
+      <Route path="/mysite" element={<Contact />} />
       <Route path="/blog" element={<Blog />}>
         <Route path="" element={<Posts />} />
         <Route path=":postSlug" element={<Post />} />
